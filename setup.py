65e2496cf517610b8397f6eb48d5f1bd0b23333b
kellerx <keller0319@gmail.com>
2018-05-10 01:50:38 +0800

    change url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tfeature',
-    version='0.11',
+    version='0.21',
     author='kellerx',
     author_email='kellerx0319@gmail.com',
     url='https://github.com/xashes/tfeature',
